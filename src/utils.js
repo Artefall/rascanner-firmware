d9a98b108109e0eb41dea99932d04a3608fe87af
artefall <maximov.artyom007@gmail.com>
2020-10-27 11:40:28 +0200

    Changed code to right code convention

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,16 @@
 module.exports = {
     processMacAddress: (chunk, lastRecord) => {
+        
         const DEFAULT_MAC_LENGTH_IN_SYMBOLS = "17";
-
-        let processableChunk = lastRecord + chunk;
-
         const NEW_LINE = '\n';
 
+        let processableChunk = lastRecord + chunk;
         const processedMacAddress = processableChunk.split(NEW_LINE);
-
         let indexOfLastProcessedMac = processedMacAddress.length - 1;
-
         let lastProcessedMacIncomplete = processedMacAddress[indexOfLastProcessedMac].length != DEFAULT_MAC_LENGTH_IN_SYMBOLS;
 
         if (lastProcessedMacIncomplete) lastRecord = processedMacAddress.pop();
 
         return processedMacAddress;
-
     }
 }
