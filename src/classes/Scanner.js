2f8a13b4f749019ccab61cf317dbb87cacbcd186
artefall <maximov.artyom007@gmail.com>
2020-11-04 00:27:48 +0200

    Insert NetCatd class and optimized code

--- src/classes/Scanner.js
+++ src/classes/Scanner.js
@@ -1,19 +1,16 @@
 const {exec} = require('child_process');
 const Processor = require('./Processor');
-
-const GREP_EXP_FIND_MAC = '([[:xdigit:]]{2}:){5}([[:xdigit:]]{2})';
+const NetCard = require('./NetCard');
 
 class Scanner{    
     constructor(interfaceToListen){
-        this.command = `sudo tcpdump -i ${interfaceToListen} -e | grep -o -E '${GREP_EXP_FIND_MAC}'`;
+        this.netCard = new NetCard(interfaceToListen);
         this.processor = new Processor();
     }
 
     run (){
-        this.macAddressStream = exec(this.command).stdout;
-        this.macAddressStream.on('data', this.processor.processMacAddressChunk);
-    }
-    
+        this.netCard.sniffPackages(this.processor.processMacAddressChunk);
+    }    
 };
 
 module.exports = Scanner;
