553d8052282acfdb73e07aa3c57b16d12471843d
artefall <maximov.artyom007@gmail.com>
2020-11-04 00:26:50 +0200

    Inplemented NetCard class

---
+++ src/classes/NetCard.js
@@ -0,0 +1,32 @@
+const Cli = require('./Cli');
+
+class NetCard{
+
+    constructor(interface){
+        this.interface = interface;
+        this.cli = new Cli();
+    }
+
+    sniffPackages(handler){
+        this.cli.sniffPackages(this.interface, handler);
+    }
+
+    monitorModeSwitchAndChangeChannels(){
+        console.log("Switch netcard to monitor mode");
+        this.cli.switchCardToMonitorMode();
+        console.log("Done");
+
+        console.log("Start to change channels");
+        this.cli.changeChannel();
+
+    }
+
+    managedModeSwitch(){
+        console.log("Switch netcard to managed mode");
+        this.cli.switchCardToManagedMode();
+        console.log("Done");
+    }
+
+}
+
+module.exports = NetCard;
