507a4c7e6a945a2fbd06361aacf60304e56ba929
artefall <maximov.artyom007@gmail.com>
2020-11-04 00:26:08 +0200

    Improved and optimized index script

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 const Scanner = require('./classes/Scanner');
-const { argv } = require('process');
-const {exec} = require('child_process');
-const START_SCRIPT_PATH = './../scripts/start.sh';
+const {argv} = require('process');
 const interfaceToListen = argv[2];
+
 const interfaceNotSet = !interfaceToListen || interfaceToListen == "";
-const startScriptCommandWithArgument = `${START_SCRIPT_PATH} ${interfaceToListen}`;
 
 if (interfaceNotSet) throw Error(`Interface wasn't set`);
 
-console.log("Run bash scripts...");
-
-exec(startScriptCommandWithArgument);
-
 console.log("NodeJS is running...");
-
 
 const scanner = new Scanner(interfaceToListen);
 scanner.run();
-
-
