4b32bc0382752365bcc2ff7599543c090ff3ecb7
artefall <maximov.artyom007@gmail.com>
2020-11-03 23:16:47 +0200

    Add exception thrower

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ const { argv } = require('process');
 const {exec} = require('child_process');
 const START_SCRIPT_PATH = './../scripts/start.sh';
 const interfaceToListen = argv[2];
+const interfaceNotSet = !interfaceToListen || interfaceToListen == "";
+const startScriptCommandWithArgument = `${START_SCRIPT_PATH} ${interfaceToListen}`;
 
-if (!interfaceToListen || interfaceToListen == "") throw Error(`Interface wasn't set`);
+if (interfaceNotSet) throw Error(`Interface wasn't set`);
 
 console.log("Run bash scripts...");
 
-exec(`${START_SCRIPT_PATH} ${interfaceToListen}`);
+exec(startScriptCommandWithArgument);
 
 console.log("NodeJS is running...");
 
 
 const scanner = new Scanner(interfaceToListen);
 scanner.run();
+
+
